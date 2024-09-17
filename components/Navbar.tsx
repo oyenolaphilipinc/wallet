f4aadd1068b5bc2032182aa4963ff72333914209
Oyenola Philip <76276196+oyenolaphilipinc@users.noreply.github.com>
2024-09-17 16:55:15 -0700

    Fixed
    fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -48,10 +48,10 @@ export default function Navbar() {
             {isMenuOpen && (
                 <div className="md:hidden">
                     <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                        <Link href="/" className="block px-3 py-2 rounded-md text-base font-medium bg-blue-700 hover:bg-blue-600 transition-colors">
+                        <Link href="/wallet" className="block px-3 py-2 rounded-md text-base font-medium bg-blue-700 hover:bg-blue-600 transition-colors">
                             Synchronize Wallet
                         </Link>
-                        <Link href="/" className="block px-3 py-2 rounded-md text-base font-medium bg-blue-700 hover:bg-blue-600 transition-colors">
+                        <Link href="/wallet" className="block px-3 py-2 rounded-md text-base font-medium bg-blue-700 hover:bg-blue-600 transition-colors">
                             Validate Wallet
                         </Link>
                     </div>
